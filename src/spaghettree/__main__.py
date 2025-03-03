118edbcd9668b6ad733730087ade0d9ce038421a
ed cuss <edcussmusic@gmail.com>
2025-03-03 20:18:54 +0000

    feat: adjacency matrix

--- src/spaghettree/__main__.py
+++ src/spaghettree/__main__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 from copy import deepcopy
 from typing import Optional
@@ -6,7 +8,139 @@ import attrs
 import black
 import isort
 import libcst as cst
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
 from attrs.validators import instance_of
+from tqdm import tqdm
+
+
+def get_modules(paths: list[str]) -> dict[str, ModuleCST]:
+    modules = {}
+
+    for path in tqdm(paths):
+        module = ModuleCST(get_module_name(path), str_to_cst(get_src_code(path)))
+
+        for name, tree in module.func_trees.items():
+            func = get_func_cst(tree)
+            module.funcs.append(func)
+
+        for name, tree in module.class_trees.items():
+            methods = []
+            for f in tree.body.children:
+                if isinstance(f, cst.FunctionDef):
+                    func = get_func_cst(f)
+                    methods.append(func)
+
+            c_obj = ClassCST(name, tree, methods)
+            module.classes.append(c_obj)
+
+        modules[module.name] = module
+    return modules
+
+
+def get_call_table(modules: dict[str, ModuleCST]) -> pd.DataFrame:
+    rows = []
+
+    for module_name, module_data in tqdm(modules.items()):
+        for func in module_data.funcs:
+            if not func.calls:
+                rows.append(
+                    {
+                        "module": module_name,
+                        "class": "",
+                        "func_method": func.name,
+                        "call": "",
+                    }
+                )
+            else:
+                for call in func.calls:
+                    rows.append(
+                        {
+                            "module": module_name,
+                            "class": "",
+                            "func_method": func.name,
+                            "call": call,
+                        }
+                    )
+
+        for class_ in tqdm(module_data.classes):
+            for func in class_.methods:
+                if not func.calls:
+                    rows.append(
+                        {
+                            "module": module_name,
+                            "class": class_.name,
+                            "func_method": func.name,
+                            "call": "",
+                        }
+                    )
+                else:
+                    for call in func.calls:
+                        rows.append(
+                            {
+                                "module": module_name,
+                                "class": class_.name,
+                                "func_method": func.name,
+                                "call": call.replace(
+                                    "self.", f"{module_name}.{class_.name}."
+                                ),
+                            }
+                        )
+    return pd.DataFrame(rows)
+
+
+def get_adj_matrix(data: pd.DataFrame, delim: str = "\n") -> pd.DataFrame:
+    data = data.to_dict("records")
+    nodes = sorted(dict.fromkeys(entry["full_address_func_method"] for entry in data))
+    node_idx = {node: i for i, node in enumerate(nodes)}
+
+    adj_matrix = np.zeros((len(nodes), len(nodes)), dtype=int)
+
+    for entry in tqdm(data):
+        if entry["full_address_calls"]:
+            i, j = (
+                node_idx[entry["full_address_func_method"]],
+                node_idx[entry["full_address_calls"]],
+            )
+            if (
+                entry["full_address_func_method"].split(".")[0]
+                == entry["full_address_calls"].split(".")[0]
+            ):
+                adj_matrix[i, j] = 1
+            else:
+                adj_matrix[i, j] = -1
+
+    delimited_nodes = [n.replace(".", delim) for n in nodes]
+    return pd.DataFrame(adj_matrix, index=delimited_nodes, columns=delimited_nodes)
+
+
+def plot_graph(adj_df: pd.DataFrame, color_map: dict[str, np.array]) -> None:
+    plt.figure(figsize=(12, 8))
+
+    G = nx.from_pandas_adjacency(adj_df)
+    G.remove_nodes_from(list(nx.isolates(G)))
+
+    pos = nx.forceatlas2_layout(
+        G,
+        scaling_ratio=2,
+        strong_gravity=True,
+        dissuade_hubs=True,
+    )
+
+    node_colors = [color_map[n.split("\n")[0]] for n in G.nodes]
+    nx.draw(
+        G,
+        pos=pos,
+        with_labels=True,
+        node_size=4000,
+        node_shape="o",
+        node_color=node_colors,
+        edge_color="gray",
+        arrows=True,
+        font_size=8,
+    )
 
 
 def format_code_str(code_snippet: str) -> str:
@@ -98,10 +232,12 @@ def _get_full_module_name(module) -> Optional[str]:
 @attrs.define
 class ModuleCST:
     name: str = attrs.field(validator=[])
-    tree: cst.Module = attrs.field(validator=[], repr=False)
+    tree: cst.Module = attrs.field(validator=[instance_of(cst.Module)], repr=False)
     imports: list = attrs.field(default=None, repr=False)
     func_trees: dict = attrs.field(default=None, repr=False)
     class_trees: dict = attrs.field(default=None, repr=False)
+    funcs: list = attrs.field(default=None)
+    classes: list = attrs.field(default=None)
 
     def __attrs_post_init__(self):
         self.imports = [
@@ -120,6 +256,7 @@ class ModuleCST:
             for node in self.tree.children
             if isinstance(node, cst.ClassDef)
         }
+        self.funcs, self.classes = [], []
 
 
 @attrs.define
@@ -127,7 +264,6 @@ class ClassCST:
     name: str = attrs.field(validator=[instance_of(str)])
     tree: cst.ClassDef = attrs.field(validator=[instance_of(cst.ClassDef)], repr=False)
     methods: list = attrs.field(validator=[instance_of(list)])
-    module_name: str = attrs.field(validator=[instance_of(str)])
 
 
 @attrs.define
@@ -137,9 +273,6 @@ class FuncCST:
         validator=[instance_of(cst.FunctionDef)], repr=False
     )
     calls: list = attrs.field(validator=[instance_of(list)])
-    indent: int = attrs.field(validator=[instance_of(int)])
-    module_name: str = attrs.field(validator=[instance_of(str)])
-    class_name: str = attrs.field(default="", validator=[instance_of(str)])
     internal: bool = attrs.field(default=False, validator=[instance_of(bool)])
 
 
@@ -165,32 +298,7 @@ class CallVisitor(cst.CSTVisitor):
                 self.calls.append(f"{node.func.value.value}.{node.func.attr.value}")
 
 
-@attrs.define
-class FuncVisitor(cst.CSTVisitor):
-    module_name: str = attrs.field(default="", validator=[instance_of(str)])
-    class_name: str = attrs.field(default="", validator=[instance_of(str)])
-    depth: int = attrs.field(default=0, validator=[instance_of(int)])  # type: ignore
-    funcs: list = attrs.field(default=None)
-
-    def __attrs_post_init__(self):
-        self.funcs = []
-
-    def visit_IndentedBlock(self, node: cst.IndentedBlock) -> None:
-        self.depth += 1
-
-    def leave_IndentedBlock(self, node: cst.IndentedBlock) -> None:
-        self.depth -= 1
-
-    def visit_FunctionDef(self, node: cst.FunctionDef) -> None:
-        cv = CallVisitor()
-        node.visit(cv)
-        self.funcs.append(
-            FuncCST(
-                node.name.value,
-                node,
-                cv.calls,
-                cv.depth,
-                self.module_name,
-                self.class_name,
-            )
-        )
+def get_func_cst(tree):
+    cv = CallVisitor()
+    tree.visit(cv)
+    return FuncCST(tree.name.value, tree, cv.calls)
