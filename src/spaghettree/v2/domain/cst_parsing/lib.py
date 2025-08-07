8a183e998c3875a9df0e9292c9bfccd0ead6fe8d
ed cuss <edcussmusic@gmail.com>
2025-08-07 23:47:26 +0100

    feat: resolve imports and calls

--- src/spaghettree/v2/domain/cst_parsing/lib.py
+++ src/spaghettree/v2/domain/cst_parsing/lib.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from copy import deepcopy
 
 import libcst as cst
 from tqdm import tqdm
@@ -45,3 +46,44 @@ def create_module_cst_objs(src_code: dict[str, str]) -> dict[str, ModuleCST]:
 
         modules[module.name] = module
     return modules
+
+
+@safe
+def resolve_module_calls(modules: dict[str, ModuleCST]) -> dict[str, ModuleCST]:
+    def resolve_calls(
+        calls: list[str], import_map: dict[str, str], func_map: dict[str, str]
+    ) -> list[str]:
+        resolved_calls: list[str] = []
+        for call in calls:
+            if resolved_call := import_map.get(call.split(".")[0]):
+                if resolved_call.split(".")[-1] != call:
+                    common_removed = ".".join(resolved_call.split(".")[:-1])
+                    resolved_calls.append(f"{common_removed}.{call}".strip("."))
+                else:
+                    resolved_calls.append(resolved_call)
+            elif resolved_call := func_map.get(call.split(".")[0]):
+                resolved_calls.append(resolved_call)
+            else:
+                resolved_calls.append(call)
+        return resolved_calls
+
+    modified_modules = {}
+
+    for name, mod in modules.items():
+        mod = deepcopy(mod)
+
+        import_map = {
+            i.as_name: f"{i.module}.{i.as_name}" if i.module != i.as_name else i.module
+            for i in mod.imports
+        }
+        func_map = {fn.name.split(".")[-1]: fn.name for fn in mod.funcs}
+
+        for fn in mod.funcs:
+            fn.calls = resolve_calls(fn.calls, import_map, func_map)
+
+        for class_ in mod.classes:
+            for fn in class_.methods:
+                fn.calls = resolve_calls(fn.calls, import_map, func_map)
+
+        modified_modules[name] = mod
+    return modified_modules
